1885b478cd4c606ffdebb340b962d5c39b5819cd
agehlot <agehlot806@gmail.com>
2025-04-28 14:42:31 +0530

    first env file updatdsd

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const PORT = process.env.PORT || 4500;
+const PORT = process.env.PORT || 3306;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
